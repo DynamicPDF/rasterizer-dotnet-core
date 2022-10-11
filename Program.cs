37b38a11691dd411a1647a6a6868c360ae2d7143
James A. Brannan <jamesabrannan@hushmail.com>
2022-10-11 16:35:45 -0400

    more changes to Program.cs, modified readme

--- Program.cs
+++ Program.cs
@@ -6,23 +6,23 @@
         {
             // Calls the Run method on all the example classes
             // The rasterized images will be saved in the applications bin folder by default
-            // PngBackgroundImageWithRgbColor.Run();
-            // PngBackgroundImageWithColorWithTransparency.Run();
-            //  GifWithWebPalette.Run();
-            //  IndexedImage.Run();
-            //JpegImage.Run();
-            //     MonochromeImage.Run();
-           //   MultiPageTiff.Run();
-            //       PdfPortfolio.Run();
-            //     PngImageWithRgbColor.Run();
-            //     PngImageWithUserPalette.Run();
-            //    RgbaImage.Run();
-            //    TiffImageColorFormat.Run();
-            //RasterizationEventHandling.Run();
-            //RasterizationEventHandling.Run2();
-            //CropImage.Run();
-          //  DitheringImage.Run();
-          //  RgbaTransparency.Run();
+            PngBackgroundImageWithRgbColor.Run();
+            PngBackgroundImageWithColorWithTransparency.Run();
+            GifWithWebPalette.Run();
+            IndexedImage.Run();
+            JpegImage.Run();
+            MonochromeImage.Run();
+            MultiPageTiff.Run();
+            PdfPortfolio.Run();
+            PngImageWithRgbColor.Run();
+            PngImageWithUserPalette.Run();
+            RgbaImage.Run();
+            TiffImageColorFormat.Run();
+            RasterizationEventHandling.Run();
+            RasterizationEventHandling.Run2();
+            CropImage.Run();
+            DitheringImage.Run();
+            RgbaTransparency.Run();
             RgbaImageCustomNaming.Run();
         }
     }
