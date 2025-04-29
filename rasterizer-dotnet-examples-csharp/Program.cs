25733e4fb477c081085cd7608be28d4db21c8889
James A. Brannan <jamesabrannan@hushmail.com>
2025-04-29 11:07:14 -0400

    additions to resources and creation of two projects

---
+++ rasterizer-dotnet-examples-csharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace rasterizer_dotnet_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PngImageExample.Run();
+        }
+
+        public static string GetResourcePath(string inputFileName)
+        {
+#pragma warning disable SYSLIB0012 // Type or member is obsolete
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+#pragma warning restore SYSLIB0012 // Type or member is obsolete
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return System.IO.Path.Combine(appRoot, "Resources", inputFileName);
+        }
+
+        public static string GetOutputDocPath(string outputfileName)
+        {
+#pragma warning disable SYSLIB0012 // Type or member is obsolete
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+#pragma warning restore SYSLIB0012 // Type or member is obsolete
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            if (!Directory.Exists(Path.Combine(appRoot, "OutputFiles")))
+            {
+                Directory.CreateDirectory(System.IO.Path.Combine(appRoot, "OutputFiles"));
+            }
+
+            return System.IO.Path.Combine(appRoot, "OutputFiles", outputfileName);
+        }
+
+    }
+}
---
+++ rasterizer-dotnet-vb/Program.vb
@@ -0,0 +1,30 @@
+Imports System
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+
+
+    Public Function GetResourcePath(inputFileName As String) As String
+        Dim exePath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+        Dim appPathMatcher As Regex = New Regex("(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)")
+        Dim appRoot As String = appPathMatcher.Match(exePath).Value
+        Return System.IO.Path.Combine(appRoot, "Resources", inputFileName)
+    End Function
+
+
+    Public Function GetOutputDocPath(outputfileName As String) As String
+        Dim exePath As String = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+        Dim appPathMatcher As Regex = New Regex("(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)")
+        Dim appRoot As String = appPathMatcher.Match(exePath).Value
+        If (Not Directory.Exists(Path.Combine(appRoot, "OutputFiles"))) Then
+            Directory.CreateDirectory(System.IO.Path.Combine(appRoot, "OutputFiles"))
+        End If
+
+        Return System.IO.Path.Combine(appRoot, "OutputFiles", outputfileName)
+    End Function
+
+End Module
